f4bb2fcd806d0f9227e9ccb8cc677ac691e00849
Wuzeiko <105318931+Wuzerer@users.noreply.github.com>
2025-12-16 16:44:19 -0300

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/aplicacion-seguridad/sw.js', { scope: '/aplicacion-seguridad/' })})}
